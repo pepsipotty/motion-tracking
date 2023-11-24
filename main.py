a2d1c4fad94418001d0fe324218166196d9996cf
pepsipotty <44318003+pepsipotty@users.noreply.github.com>
2023-11-24 16:21:11 -0500

    change hybrid function to calculate proportion instead of count and include test function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from skimage import io
 import numpy as np 
 import matplotlib.pyplot as plt
 import math
+import cv2
 
 import scipy.ndimage
 from scipy.linalg import eigh
@@ -17,6 +18,11 @@ target2 = skimage.io.imread('target2.jpg')
 plt.imshow(target2)
 plt.show()
 print(target2.shape)
+
+target3 = skimage.io.imread('target3.jpg')
+plt.imshow(target3)
+plt.show()
+print(target3.shape)
 
 
 def covarience_tracking(im, target):
@@ -64,24 +70,28 @@ def covarience_tracking(im, target):
     return minDiff, minpos
 
 
-def covarience_tracking_robust(im, target, target2):
+def covarience_tracking_robust(im, target, target2, target3):
+    
     # Select the best match beteen two targets and make it robust
     minDiff1, minpos1 = covarience_tracking(im, target)
     minDiff2, minpos2 = covarience_tracking(im, target2)
-    if minDiff1 <= minDiff2:
-        fig, ax = plt.subplots()
-        ax.imshow(im)
-        rect = patches.Rectangle((minpos1[1], minpos1[0]), target.shape[1], target.shape[0], linewidth=1, edgecolor='r',
-                                 facecolor='none')
-        ax.add_patch(rect)
-        plt.show()
+    minDiff3, minpos3 = covarience_tracking(im, target3)
+    
+    if minDiff1 <= minDiff2 and minDiff1 <= minDiff3:
+        best_pos = minpos1
+        best_target = target
     else:
-        fig, ax = plt.subplots()
-        ax.imshow(im)
-        rect = patches.Rectangle((minpos2[1], minpos2[0]), target2.shape[1], target2.shape[0], linewidth=1,
-                                 edgecolor='r', facecolor='none')
-        ax.add_patch(rect)
-        plt.show()
+        if minDiff2 <= minDiff1 and minDiff2 <= minDiff3:
+            best_pos = minpos2
+            best_target = target2
+        else:
+            best_pos = minpos3
+            best_target = target3
+    
+    
+    best_patch = im[best_pos[0]:(best_pos[0]+best_target.shape[0]),best_pos[1]:(best_pos[1]+best_target.shape[1])]
+        
+    return best_patch,best_target
 
 
 
@@ -113,26 +123,62 @@ def ncc_tracking(im, target):
 
             if ncc > max_corr:
                 max_corr = ncc
-                max_pos = (x, y)
+                
+                # I think the below should be (y,x) not (x, y)?
+                max_pos = (y, x)
 
     return ncc, max_pos
 
-# does not work. Runs afoul as some second image has a higher correlation than the first, 
-# despite the first being a direct cutout from the image
-# def ncc_tracking_robust(im, target, target2):
-#     ncc_1, max_pos_1 = ncc_tracking(im, target)
-#     ncc_2, max_pos_2 = ncc_tracking(im, target2)
+def ncc_tracking_robust(im, target, target2, target3):
+    ncc_1, max_pos_1 = ncc_tracking(im, target)
+    ncc_2, max_pos_2 = ncc_tracking(im, target2)
+    ncc_3, max_pos_3 = ncc_tracking(im, target3)
+    
+    if ncc_1 >= ncc_2 and ncc_1 >= ncc_3:
+        best_pos = max_pos_1
+        best_target = target
+    else:
+        if ncc_2 >= ncc_1 and ncc_2 >= ncc_3:
+            best_pos = max_pos_2
+            best_target = target2
+        else:
+            best_pos = max_pos_3
+            best_target = target3  
+            
+    
+    
+    # Check whether this dimension is correct
+    best_patch = im[best_pos[0]:(best_pos[0]+best_target.shape[0]),best_pos[1]:(best_pos[1]+best_target.shape[1])]
+        
+    return best_patch,best_target
+    
+def count_near_black_and_white_pixels(im):
 
-#     if ncc_1 > ncc_2:
-#         best_ncc = ncc_1
-#         best_pos = max_pos_1
-#         best_target = 0
-#     else:
-#         best_ncc = ncc_2
-#         best_pos = max_pos_2
-#         best_target = 1
+    image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
-#     return best_ncc, best_pos, best_target
+    black_threshold = 30
+    white_threshold = 225
+
+    near_black_count = np.sum(np.all(image < black_threshold, axis=-1))
+
+    near_white_count = np.sum(np.all(image > white_threshold, axis=-1))
+    
+    proportion = (near_black_count + near_white_count) / (image.shape[0] * image.shape[1])
+
+    return proportion
+
+def hybrid_covarience_ncc(im, target, target2, target3):
+    best_patch, best_target = covarience_tracking_robust(im, target, target2, target3)
+    best_patch2, best_target2 = ncc_tracking_robust(im, target, target2, target3)
+
+    proportion1 = count_near_black_and_white_pixels(best_patch)
+    proportion2 = count_near_black_and_white_pixels(best_patch2)
+    
+    if proportion1 >= proportion2:
+        return best_patch
+    else:
+        return best_patch2
+    
 
 def ncc_tracking_demo():
     imDemo = []
@@ -152,7 +198,57 @@ def ncc_tracking_demo():
         ax.add_patch(rect)
         plt.show()
 
-ncc_tracking_demo()
+
+def testResults(target1, target2, target3):
+    # 100 random numbers between the range of 0 and 1277
+    random_numbers = np.random.randint(0, 1278, size=100)
+
+    images = []
+    for i in random_numbers:
+        img = skimage.io.imread(f'./Dataset/image{i}.jpg')
+        best_patch = hybrid_covarience_ncc(img, target1, target2, target3)
+        print(f"image{i}: {best_patch.shape}")
+
+        images.append(best_patch)
+
+    rows = 10
+    cols = 10
+    fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i])
+        ax.axis('off')
+
+    plt.show()
+
+testResults(target, target2, target3)
+
+
+# im = skimage.io.imread('Dataset/image0.jpg')
+# im2 = skimage.io.imread('Dataset/image596.jpg')
+# im3 = skimage.io.imread('Dataset/image794.jpg')
+# best_patch = hybrid_covarience_ncc(im, target, target2, target3)
+
+# print(best_patch.shape)
+
+# fig,ax = plt.subplots()
+# ax.imshow(best_patch)
+# plt.show()
+
+# best_patch = hybrid_covarience_ncc(im2, target, target2, target3)
+
+# print(best_patch.shape)
+
+# fig,ax = plt.subplots()
+# ax.imshow(best_patch)
+# plt.show()
+
+# best_patch = hybrid_covarience_ncc(im3, target, target2, target3)
+
+# print(best_patch.shape)
+
+# fig,ax = plt.subplots()
+# ax.imshow(best_patch)
+# plt.show()
 
 # im2 = skimage.io.imread('Dataset/image0.jpg')
 # covarience_tracking_robust(im, target, target2)
